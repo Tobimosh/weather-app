3d7b6578acfcb22eab544291bd19573caf6ed5ff
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2024-04-12 16:58:19 +0100

    map error fix

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -110,102 +110,201 @@ export class MapComponent implements OnInit {
   //     this.getLocation();
   //   }
   // }
-  public map: any;
-  private centroid: L.LatLngExpression;
-  private currentLocationMarker: any;
+  // public map: any;
+  // private centroid: L.LatLngExpression;
+  // private currentLocationMarker: any;
 
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object,
-    private spinner: NgxSpinnerService
-  ) {
-    this.centroid = [0, 0];
+  // constructor(
+  //   @Inject(PLATFORM_ID) private platformId: Object,
+  //   private spinner: NgxSpinnerService
+  // ) {
+  //   this.centroid = [0, 0];
+  // }
+
+  // private async initMap(): Promise<void> {
+  //   if (isPlatformBrowser(this.platformId)) {
+  //     const L = await import('leaflet');
+  //     this.map = L.map('map', {
+  //       center: this.centroid,
+  //       zoom: 10,
+  //       zoomControl: false,
+  //     }).fitWorld();
+
+  //     const tiles = L.tileLayer(
+  //       'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+  //       {
+  //         maxZoom: 18,
+  //         minZoom: 6,
+  //         attribution: '© OpenStreetMap',
+  //       }
+  //     );
+  //     const markerIcon = L.icon({
+  //       iconUrl: '../../assets/images/marker.svg',
+  //       iconSize: [25, 41],
+  //     });
+
+  //     const jittery = Array(5)
+  //       .fill(this.centroid)
+  //       .map((x) => [
+  //         x[0] + (Math.random() - 0.2),
+  //         x[1] + (Math.random() - 0.2),
+  //       ])
+  //       .map((x) =>
+  //         L.marker(x as L.LatLngExpression, { icon: markerIcon }).bindPopup(
+  //           'A random location on the map'
+  //         )
+  //       )
+  //       .forEach((x) => x.addTo(this.map));
+  //     tiles.addTo(this.map);
+  //   }
+  // }
+
+  // getLocation() {
+  //   if (isPlatformBrowser(this.platformId)) {
+  //     this.spinner.show();
+  //     if ('geolocation' in navigator) {
+  //       navigator.geolocation.getCurrentPosition(
+  //         (position: GeolocationPosition) => {
+  //           this.centroid = [
+  //             position.coords.latitude,
+  //             position.coords.longitude,
+  //           ];
+  //           this.initMap().then(() => {
+  //             this.addCurrentLocationMarker(this.centroid);
+  //             this.map.setView(this.centroid, 10);
+  //             this.spinner.hide();
+  //           });
+  //         },
+  //         (error: GeolocationPositionError) => {
+  //           console.error('Error getting geolocation:', error);
+  //           this.spinner.hide();
+  //         }
+  //       );
+  //     } else {
+  //       console.error('Geolocation is not supported by this browser.');
+  //       this.spinner.hide();
+  //     }
+  //   }
+  // }
+
+  // private async addCurrentLocationMarker(
+  //   coordinates: L.LatLngExpression
+  // ): Promise<void> {
+  //   if (isPlatformBrowser(this.platformId)) {
+  //     const L = await import('leaflet');
+  //     const markerIcon = L.icon({
+  //       iconUrl: '../../assets/images/live-location.svg',
+  //       iconSize: [50, 50],
+  //     });
+  //     this.currentLocationMarker = L.marker(coordinates, {
+  //       icon: markerIcon,
+  //     }).addTo(this.map);
+  //     this.currentLocationMarker.bindPopup('Where you at').openPopup();
+  //   }
+  // }
+
+  // ngOnInit() {
+  //   if (isPlatformBrowser(this.platformId)) {
+  //     this.getLocation();
+  //   }
+  // }
+
+  public map: L.Map;
+private centroid: L.LatLngExpression;
+private currentLocationMarker: any;
+
+constructor(
+  @Inject(PLATFORM_ID) private platformId: Object,
+  private spinner: NgxSpinnerService
+) {
+  this.centroid = [0, 0];
+}
+
+private async initMap(): Promise<void> {
+  if (isPlatformBrowser(this.platformId)) {
+    const L = await import('leaflet');
+    this.map = L.map('map', {
+      center: this.centroid,
+      zoom: 10,
+      zoomControl: false,
+    }).fitWorld();
+
+    const tiles = L.tileLayer(
+      'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 6,
+        attribution: '© OpenStreetMap',
+      }
+    );
+    const markerIcon = L.icon({
+      iconUrl: '../../assets/images/marker.svg',
+      iconSize: [25, 41],
+    });
+
+    const jittery = Array(5)
+      .fill(this.centroid)
+      .map((x) => [
+        x[0] + (Math.random() - 0.2),
+        x[1] + (Math.random() - 0.2),
+      ])
+      .map((x) =>
+        L.marker(x as L.LatLngExpression, { icon: markerIcon }).bindPopup(
+          'A random location on the map'
+        )
+      )
+      .forEach((x) => x.addTo(this.map));
+    tiles.addTo(this.map);
   }
+}
 
-  private async initMap(): Promise<void> {
-    if (isPlatformBrowser(this.platformId)) {
-      const L = await import('leaflet');
-      this.map = L.map('map', {
-        center: this.centroid,
-        zoom: 10,
-        zoomControl: false,
-      }).fitWorld();
-
-      const tiles = L.tileLayer(
-        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        {
-          maxZoom: 18,
-          minZoom: 6,
-          attribution: '© OpenStreetMap',
+getLocation() {
+  if (isPlatformBrowser(this.platformId)) {
+    this.spinner.show();
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          this.centroid = [
+            position.coords.latitude,
+            position.coords.longitude,
+          ];
+          this.initMap().then(() => {
+            this.addCurrentLocationMarker(this.centroid);
+            this.map.setView(this.centroid, 10);
+            this.spinner.hide();
+          });
+        },
+        (error: GeolocationPositionError) => {
+          console.error('Error getting geolocation:', error);
+          this.spinner.hide();
         }
       );
-      const markerIcon = L.icon({
-        iconUrl: '../../assets/images/marker.svg',
-        iconSize: [25, 41],
-      });
-
-      const jittery = Array(5)
-        .fill(this.centroid)
-        .map((x) => [
-          x[0] + (Math.random() - 0.2),
-          x[1] + (Math.random() - 0.2),
-        ])
-        .map((x) =>
-          L.marker(x as L.LatLngExpression, { icon: markerIcon }).bindPopup(
-            'A random location on the map'
-          )
-        )
-        .forEach((x) => x.addTo(this.map));
-      tiles.addTo(this.map);
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+      this.spinner.hide();
     }
   }
+}
 
-  getLocation() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.spinner.show();
-      if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(
-          (position: GeolocationPosition) => {
-            this.centroid = [
-              position.coords.latitude,
-              position.coords.longitude,
-            ];
-            this.initMap().then(() => {
-              this.addCurrentLocationMarker(this.centroid);
-              this.map.setView(this.centroid, 10);
-              this.spinner.hide();
-            });
-          },
-          (error: GeolocationPositionError) => {
-            console.error('Error getting geolocation:', error);
-            this.spinner.hide();
-          }
-        );
-      } else {
-        console.error('Geolocation is not supported by this browser.');
-        this.spinner.hide();
-      }
-    }
+private async addCurrentLocationMarker(
+  coordinates: L.LatLngExpression
+): Promise<void> {
+  if (isPlatformBrowser(this.platformId)) {
+    const L = await import('leaflet');
+    const markerIcon = L.icon({
+      iconUrl: '../../assets/images/live-location.svg',
+      iconSize: [50, 50],
+    });
+    this.currentLocationMarker = L.marker(coordinates, {
+      icon: markerIcon,
+    }).addTo(this.map);
+    this.currentLocationMarker.bindPopup('Where you at').openPopup();
   }
+}
 
-  private async addCurrentLocationMarker(
-    coordinates: L.LatLngExpression
-  ): Promise<void> {
-    if (isPlatformBrowser(this.platformId)) {
-      const L = await import('leaflet');
-      const markerIcon = L.icon({
-        iconUrl: '../../assets/images/live-location.svg',
-        iconSize: [50, 50],
-      });
-      this.currentLocationMarker = L.marker(coordinates, {
-        icon: markerIcon,
-      }).addTo(this.map);
-      this.currentLocationMarker.bindPopup('Where you at').openPopup();
-    }
+ngOnInit() {
+  if (isPlatformBrowser(this.platformId)) {
+    this.getLocation();
   }
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.getLocation();
-    }
-  }
+}
 }
